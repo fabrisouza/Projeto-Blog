91c21cdaa6faf86803c72c57853d14b574afae64
fabrisouza <88212329+fabrisouza@users.noreply.github.com>
2021-09-17 10:27:50 -0300

    models inserido

--- churrasco/urls.py
+++ churrasco/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+import churrasco
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('churrasco', views.churrasco, name='churrasco')
 ]
--- churrasco/views.py
+++ churrasco/views.py
@@ -7,3 +7,6 @@ def index(request):
     return render(request, 'index.html')
 
 
+
+def churrasco(request):
+    return render(request, 'churrasco.html')
